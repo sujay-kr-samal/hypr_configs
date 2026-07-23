9aa8bab6accdb6d9d869a07d707bd798fc9a3c7d
sujay kr samal <sujaykrsamal@gmail.com>
2026-07-23 15:52:22 +0530

    Add files via upload

---
+++ nvim/matugen_colors.lua
@@ -0,0 +1,35 @@
+return {
+  -- Core Backgrounds (Deepest Darks)
+  crust     = "#140c0b",
+  mantle    = "#231918",
+  base      = "#1a1110",
+  
+  -- UI Elements & Overlays
+  surface0  = "#271d1c",
+  surface1  = "#322826",
+  surface2  = "#3d3231",
+  overlay0  = "#534341",
+  overlay1  = "#a08c89",
+  overlay2  = "#d8c2be",
+  
+  -- Text & Foreground (Brightest Light)
+  subtext0  = "#d8c2be",
+  subtext1  = "#f1dedc",
+  text      = "#f1dedc",
+  
+  -- Syntax Highlights (Bright Accents ONLY)
+  rosewater = "#ffb4a9",
+  flamingo  = "#dec38c",
+  pink      = "#dec38c",
+  mauve     = "#dec38c",
+  red       = "#ffb4ab",
+  maroon    = "#ffb4ab",
+  peach     = "#e7bdb7",
+  yellow    = "#e7bdb7",
+  green     = "#ffb4a9",
+  teal      = "#e7bdb7",
+  sky       = "#ffb4a9",
+  sapphire  = "#e7bdb7",
+  blue      = "#ffb4a9",
+  lavender  = "#dec38c",
+}
